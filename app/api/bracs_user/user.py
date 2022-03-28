3fa4ee79cacc89f303a574d154c241c2ffd7f723
nick-lennox <>
2022-03-28 18:51:34 -0400

    cleaned up responses and error handling

--- app/api/bracs_user/user.py
+++ app/api/bracs_user/user.py
@@ -25,89 +25,38 @@ def post_user():
 
     db_session.add(user)
     db_session.commit()
+    return response({"userID":user.id}, 200)
   except Exception as e:
     # if username/email already exists return default response
     raise UnableToCompleteAction(e)
 
-  return response({"userID":user.id}, 200)
-
-
-
 
 @api.route('/user/<int:userID>', methods=['GET'])
 def get_user(userID):
-
-  authUser = False
-
   try:
     user = db_session.query(User).filter_by(id=userID).one_or_none()
-    return user 
+    return response(user, 200) 
   except Exception as e:
     raise UnableToCompleteAction(e)
-  # try:
-  #   authUser = processToken(request.headers["Authorization"])
-  # except:
-  #   return defaultResponse()
-
-  # # bad token
-  # if not authUser:
-  #   return defaultResponse()
-
-  # # user is not an admin and is not getting themselves
-  # if not authUser.isAdmin and authUser.id != userID:
-  #   return defaultResponse()
-
-  # user = db_session.query(User).filter_by(id=userID).one_or_none()
-
-  # if user:
-  #   return response(user, 200)
-  
-  # return response({"error":"user not found"}, 200)
-
-# @app.route('/api/modify_user', methods=['PUT'])
-# def modify_user():
-#   data = json.loads(request.data)
-#   userID = data['userID']
-#   user = User.query.filter_by(id=userID).first()
 
 @api.route('/login', methods=['POST'])
 def login():
-
   identifier = None
   password = None
-
   try:
     data = json.loads(request.data)
     identifier = data['identifier']
-    password = data['password']
-  
-  except Exception as e:
-    return UnableToCompleteAction(e)
-
-  userFound = False
-
-  # first try logging in with username 
-  user = db_session.query(User).filter_by(username=identifier).one_or_none()
-
-  if user:
-    userFound = True
-
-  else:
-    # now try logging in with email
-    user = db_session.query(User).filter_by(email=identifier).one_or_none()
-    if user:
-      userFound = True
-
-  if userFound:
-
-    # if user found check password match & give token
-    if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+    password = data['password'] # mad secure bruh
+    user = db_session.query(User).filter_by(username=identifier).one_or_none()
+    if user is None:
+      user = db_session.query(User).filter_by(email=identifier).one_or_none()
+    if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
         token = Token(userID=user.id, token=secrets.token_hex(256))
         db.session.add(token)
         db.session.commit()
 
         return response({"token":token.token, "userID":user.id}, 200)
-
-  # if username/email not real or password match is bad return default response
-
-  return defaultResponse()
+    else:
+      raise Exception('Error: Could not verify user')
+  except Exception as e:
+    raise UnableToCompleteAction(e)
